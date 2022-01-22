5ef3493cb0bb9a0171325128b176700b391c0577
Hammad <muhammadhammadali112233@gmail.com>
2022-01-22 21:31:32 +0500

    Add the Hereoku file script

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const path = require('path'); 
-const express=require('express');
-const app=express();
-const port=3000;
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const staticPath=path.join(__dirname,'./Company')  
+const staticPath = path.join(__dirname, './Company')
 
-console.log(__dirname )
+console.log(__dirname)
 app.use(express.static(staticPath));
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello From the Express Server');
 })
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`App is Running at ${port} port`)
 })
