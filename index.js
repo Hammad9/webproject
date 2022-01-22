abdace17bc5824bad1ecb0c5086cf00330bd9f00
Hammad <muhammadhammadali112233@gmail.com>
2022-01-22 14:47:45 +0500

    My first node js Project

---
+++ index.js
@@ -0,0 +1,19 @@
+const path = require('path'); 
+const express=require('express');
+const app=express();
+const port=3000;
+
+const staticPath=path.join(__dirname,'./Company')  
+
+console.log(__dirname )
+app.use(express.static(staticPath));
+
+app.get('/', (req,res)=>{
+    res.send('Hello From the Express Server');
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`App is Running at ${port} port`)
+})
